9038b7fe22273eaa662af4b5fbf0aff7cda16697
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-02-29 00:53:55 +0000

    Exercise 2.3

--- lab2/lab2_2/src/test/java/integration/AddressResolverIT.java
+++ lab2/lab2_2/src/test/java/integration/AddressResolverIT.java
@@ -1,36 +1,41 @@
 package integration;
 
+import connection.ISimpleHttpClient;
+import connection.TqsBasicHttpClient;
+import geocoding.Address;
+import geocoding.AddressResolverService;
 import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AddressResolverIT {
+    AddressResolverService resolver;
+    ISimpleHttpClient httpClient = new TqsBasicHttpClient();
+
     @BeforeEach
     public void init() {
+        resolver = new AddressResolverService(httpClient);
     }
 
-
-    @Disabled
     @Test
     public void whenGoodCoordidates_returnAddress() throws IOException, URISyntaxException, ParseException {
+        Optional<Address> result = resolver.findAddressForLocation(40.63436, -8.65616);
+        Address expected = new Address("Avenida da Universidade", "Aveiro", "3810-489", "");
 
-        //todo
-
-        // repeat the same tests conditions from AddressResolverTest, without mocks
-
+        assertTrue(result.isPresent());
+        assertEquals(expected, result.get());
     }
 
-    @Disabled
     @Test
     public void whenBadCoordidates_thenReturnNoValidAddrress() throws IOException, URISyntaxException, ParseException {
-
-        //todo
-        // repeat the same tests conditions from AddressResolverTest, without mocks
-
+        Optional<Address> result = resolver.findAddressForLocation(-361, -361);
+        // verify no valid result
+        assertFalse(result.isPresent());
     }
-
 }
