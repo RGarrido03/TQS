c40314e4c34c010885e607e66f78989c1a8b7cf5
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-03-29 00:19:15 +0000

    Make TripSearchParameters serializable

--- HW1/backend/src/main/java/pt/ua/deti/tqs/backend/specifications/trip/TripSearchParameters.java
+++ HW1/backend/src/main/java/pt/ua/deti/tqs/backend/specifications/trip/TripSearchParameters.java
@@ -5,13 +5,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.time.LocalDateTime;
+
+@SuppressWarnings("java:S1068")
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class TripSearchParameters {
+public class TripSearchParameters implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 2405172041950251807L;
+
     private Long departure;
     private Long arrival;
     private LocalDateTime departureTime;
