2ba52d659417e7ffa5915e11b4eb99195b0513d8
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-03-29 02:05:41 +0000

    Split currency and api services

---
+++ HW1/backend/src/main/java/pt/ua/deti/tqs/backend/services/ApiService.java
@@ -0,0 +1,40 @@
+package pt.ua.deti.tqs.backend.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import pt.ua.deti.tqs.backend.helpers.Currency;
+import pt.ua.deti.tqs.backend.helpers.CurrencyApiResponse;
+
+import java.util.Map;
+
+@Service
+@Slf4j
+public class ApiService {
+    private final String apiKey;
+    private final RestTemplate restTemplate;
+
+    public ApiService(@Value("${currencyapi.apikey:null}") String apiKey, RestTemplate restTemplate) {
+        this.apiKey = apiKey;
+        this.restTemplate = restTemplate;
+    }
+
+    @Cacheable("rates")
+    public Map<Currency, Double> fetchRates() {
+        log.info("Fetching rates");
+        CurrencyApiResponse forObject = restTemplate.getForEntity(
+                "https://api.freecurrencyapi.com/v1/latest?apikey=" + apiKey + "&base_currency=" + Currency.EUR,
+                CurrencyApiResponse.class).getBody();
+        return forObject != null ? forObject.getData() : null;
+    }
+
+    @CacheEvict(value = "rates", allEntries = true)
+    @Scheduled(fixedRate = 3600000)
+    public void emptyRatesCache() {
+        log.info("Emptying rates cache");
+    }
+}
--- HW1/backend/src/main/java/pt/ua/deti/tqs/backend/services/CurrencyService.java
+++ HW1/backend/src/main/java/pt/ua/deti/tqs/backend/services/CurrencyService.java
@@ -1,55 +1,22 @@
 package pt.ua.deti.tqs.backend.services;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.scheduling.annotation.Scheduled;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import pt.ua.deti.tqs.backend.helpers.Currency;
-import pt.ua.deti.tqs.backend.helpers.CurrencyApiResponse;
 
 import java.util.Map;
 
 @Service
-@Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
-@Slf4j
+@AllArgsConstructor
 public class CurrencyService {
-    public final String apiKey;
-    private final RestTemplate restTemplate;
-    private final CurrencyService self;
-
-    public CurrencyService(@Value("${currencyapi.apikey}") String apiKey, RestTemplate restTemplate, CurrencyService self) {
-        this.apiKey = apiKey;
-        this.restTemplate = restTemplate;
-        this.self = self;
-    }
-
-    @Cacheable("rates")
-    public Map<Currency, Double> fetchRates() {
-        log.info("Fetching rates");
-        CurrencyApiResponse forObject = restTemplate.getForEntity(
-                "https://api.freecurrencyapi.com/v1/latest?apikey=" + apiKey + "&base_currency=" + Currency.EUR,
-                CurrencyApiResponse.class).getBody();
-        return forObject != null ? forObject.getData() : null;
-    }
-
-    @CacheEvict(value = "rates", allEntries = true)
-    @Scheduled(fixedRate = 3600000)
-    public void refreshRatesCache() {
-        log.info("Emptying rates cache");
-        self.fetchRates();
-    }
+    private final ApiService apiService;
 
     public double convertEurToCurrency(double euros, Currency currency) {
         if (currency == Currency.EUR) {
             return euros;
         }
 
-        Map<Currency, Double> response = self.fetchRates();
+        Map<Currency, Double> response = apiService.fetchRates();
         return euros * response.get(currency);
     }
 
@@ -58,7 +25,7 @@ public class CurrencyService {
             return amount;
         }
 
-        Map<Currency, Double> response = self.fetchRates();
+        Map<Currency, Double> response = apiService.fetchRates();
         return amount / response.get(currency);
     }
 }
