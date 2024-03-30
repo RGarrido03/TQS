01b37d6e9aef0e6ce204eb4a1cee515be7d46326
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-03-30 12:53:13 +0000

    Add Tanstack's React Query

--- HW1/frontend/app/providers.tsx
+++ HW1/frontend/app/providers.tsx
@@ -2,13 +2,19 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useState } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <NextUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="system">
-        {children}
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
       </NextThemesProvider>
     </NextUIProvider>
   );
