cabba3c77238dc5f6852d353e26791316d04a65d
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-04-07 00:20:58 +0100

    Set new values in serial sequence in inserted data

--- HW1/backend/src/main/resources/db/migration/V002__DATA.sql
+++ HW1/backend/src/main/resources/db/migration/V002__DATA.sql
@@ -33,9 +33,6 @@ VALUES (1, '2024-04-06 08:00:00', '2024-04-06 06:00:00', 50, 10.00, 3, 1, 1),
 
 
 -- Set serial sequence
-SELECT pg_catalog.setval('public.bus_id_seq', 1, true);
+SELECT pg_catalog.setval('public.bus_id_seq', 4, true);
 SELECT pg_catalog.setval('public.city_id_seq', 4, true);
-SELECT pg_catalog.setval('public.reservation_id_seq', 0, true);
-SELECT pg_catalog.setval('public.trip_id_seq', 5, true);
-SELECT pg_catalog.setval('public.user_id_seq', 0, true);
-
+SELECT pg_catalog.setval('public.trip_id_seq', 18, true);
