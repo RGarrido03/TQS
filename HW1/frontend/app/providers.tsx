7284bd4f5f820c654991f0891f0457919f857b68
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-04-07 19:56:47 +0100

    I'm so sick of this

--- HW1/frontend/app/providers.tsx
+++ HW1/frontend/app/providers.tsx
@@ -5,6 +5,7 @@ import { NextUIProvider } from "@nextui-org/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { useState } from "react";
+import { CookiesProvider } from "next-client-cookies/server";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
@@ -12,9 +13,11 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="system">
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
+        <CookiesProvider>
+          <QueryClientProvider client={queryClient}>
+            {children}
+          </QueryClientProvider>
+        </CookiesProvider>
       </NextThemesProvider>
     </NextUIProvider>
   );
