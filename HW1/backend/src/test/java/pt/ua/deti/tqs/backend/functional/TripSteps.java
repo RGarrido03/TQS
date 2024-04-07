f3671593a19fadcb9e5e8540eaa0ee8ce9da47f2
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-04-07 18:28:05 +0100

    Add initial test

---
+++ HW1/backend/src/test/java/pt/ua/deti/tqs/backend/functional/TripSteps.java
@@ -0,0 +1,15 @@
+package pt.ua.deti.tqs.backend.functional;
+
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+
+public class TripSteps {
+    private WebDriver driver;
+
+    @When("I navigate to {string}")
+    public void iNavigateTo(String url) {
+        driver = WebDriverManager.firefoxdriver().create();
+        driver.get(url);
+    }
+}
---
+++ HW1/backend/src/test/resources/pt/ua/deti/tqs/backend/functional/trip.feature
@@ -0,0 +1,4 @@
+Feature: Book a trip
+
+  Scenario: Successful trip booking
+    Given I navigate to "http://localhost"
