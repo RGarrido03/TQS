7fa4b84b9bbd2f1b762fb9504f99c34b6ab349d1
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-04-07 18:27:17 +0100

    Add cucumber base class

---
+++ HW1/backend/src/test/java/pt/ua/deti/tqs/backend/functional/CucumberTest.java
@@ -0,0 +1,16 @@
+package pt.ua.deti.tqs.backend.functional;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@SuppressWarnings("java:S2187")
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("pt/ua/deti/tqs/backend/functional")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "pt.ua.deti.tqs.backend.functional")
+public class CucumberTest {
+}
