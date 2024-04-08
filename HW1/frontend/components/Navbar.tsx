0442d0e961f86fcb1cfa9596fca9640b192bcfe7
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-04-08 01:07:57 +0100

    Add currency to navbar

--- HW1/frontend/components/Navbar.tsx
+++ HW1/frontend/components/Navbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Navbar,
   NavbarBrand,
@@ -5,31 +7,32 @@ import {
   NavbarItem,
   Link,
   Button,
+  Select,
+  SelectItem,
 } from "@nextui-org/react";
 
+import { Currency, currencyCodes } from "@/types/currency";
+import { useCookies } from "next-client-cookies";
+import { useEffect, useState } from "react";
+
 export default function NavbarDesign() {
+  const cookies = useCookies();
+
+  const [currency, setCurrency] = useState<Currency>(
+    (cookies.get("currency") as Currency) || "EUR"
+  );
+
+  useEffect(() => {
+    if (cookies.get("currency") === undefined) {
+      cookies.set("currency", "EUR");
+    }
+  }, [cookies]);
+
   return (
     <Navbar>
       <NavbarBrand>
-        <p className="font-bold text-inherit">TripFinder</p>
+        <p className="text-xl font-bold text-inherit">TripFinder</p>
       </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
           <Link href="#">Login</Link>
@@ -38,6 +41,26 @@ export default function NavbarDesign() {
           <Button as={Link} color="primary" href="#" variant="flat">
             Sign Up
           </Button>
+        </NavbarItem>
+        <NavbarItem>
+          <Select
+            label="Currency"
+            size="sm"
+            variant="flat"
+            className="w-28"
+            selectedKeys={[currency]}
+            onChange={(event) => {
+              const currency = event.target.value as Currency;
+              setCurrency(currency);
+              cookies.set("currency", currency);
+            }}
+          >
+            {Object.values(currencyCodes).map((currency) => (
+              <SelectItem key={currency} value={currency}>
+                {currency}
+              </SelectItem>
+            ))}
+          </Select>
         </NavbarItem>
       </NavbarContent>
     </Navbar>
--- HW1/frontend/types/currency.ts
+++ HW1/frontend/types/currency.ts
@@ -33,6 +33,42 @@ export type Currency =
   | "USD"
   | "ZAR";
 
+export const currencyCodes: Currency[] = [
+  "AUD",
+  "BGN",
+  "BRL",
+  "CAD",
+  "CHF",
+  "CNY",
+  "CZK",
+  "DKK",
+  "EUR",
+  "GBP",
+  "HKD",
+  "HRK",
+  "HUF",
+  "IDR",
+  "ILS",
+  "INR",
+  "ISK",
+  "JPY",
+  "KRW",
+  "MXN",
+  "MYR",
+  "NOK",
+  "NZD",
+  "PHP",
+  "PLN",
+  "RON",
+  "RUB",
+  "SEK",
+  "SGD",
+  "THB",
+  "TRY",
+  "USD",
+  "ZAR",
+];
+
 export type CurrencyParams = {
   currency?: Currency;
 };
