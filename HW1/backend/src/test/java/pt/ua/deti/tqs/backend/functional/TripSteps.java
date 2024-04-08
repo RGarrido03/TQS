87980c354cc39f650a0969780df91c4d9e38e9bb
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-04-08 20:01:08 +0100

    Refactor Firefox dirver

--- HW1/backend/src/test/java/pt/ua/deti/tqs/backend/functional/TripSteps.java
+++ HW1/backend/src/test/java/pt/ua/deti/tqs/backend/functional/TripSteps.java
@@ -1,15 +1,22 @@
 package pt.ua.deti.tqs.backend.functional;
 
 import io.cucumber.java.en.When;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class TripSteps {
     private WebDriver driver;
 
     @When("I navigate to {string}")
     public void iNavigateTo(String url) {
-        driver = WebDriverManager.firefoxdriver().browserInDocker().create();
+        FirefoxBinary binary = new FirefoxBinary();
+        binary.addCommandLineOptions("-headless");
+        FirefoxOptions options = new FirefoxOptions();
+        options.setBinary(binary);
+
+        driver = new FirefoxDriver(options);
         driver.get(url);
     }
 }
