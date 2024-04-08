561fac4146d698305806fc7c95468fc339ef7865
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-04-08 20:53:25 +0100

    Test reduced code in firefox instantiation

--- HW1/backend/src/test/java/pt/ua/deti/tqs/backend/functional/TripSteps.java
+++ HW1/backend/src/test/java/pt/ua/deti/tqs/backend/functional/TripSteps.java
@@ -2,21 +2,14 @@ package pt.ua.deti.tqs.backend.functional;
 
 import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class TripSteps {
     private WebDriver driver;
 
     @When("I navigate to {string}")
     public void iNavigateTo(String url) {
-        FirefoxBinary binary = new FirefoxBinary();
-        binary.addCommandLineOptions("-headless");
-        FirefoxOptions options = new FirefoxOptions();
-        options.setBinary(binary);
-
-        driver = new FirefoxDriver(options);
+        driver = new FirefoxDriver();
         driver.get(url);
     }
 }
