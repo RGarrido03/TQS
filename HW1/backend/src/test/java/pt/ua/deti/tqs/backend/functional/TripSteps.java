ae576dc7778f220a918fa14101cb0ec095816e04
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-04-08 21:01:02 +0100

    Headless Firefox

--- HW1/backend/src/test/java/pt/ua/deti/tqs/backend/functional/TripSteps.java
+++ HW1/backend/src/test/java/pt/ua/deti/tqs/backend/functional/TripSteps.java
@@ -3,13 +3,17 @@ package pt.ua.deti.tqs.backend.functional;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class TripSteps {
     private WebDriver driver;
 
     @When("I navigate to {string}")
     public void iNavigateTo(String url) {
-        driver = new FirefoxDriver();
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("-headless");
+
+        driver = new FirefoxDriver(options);
         driver.get(url);
     }
 }
