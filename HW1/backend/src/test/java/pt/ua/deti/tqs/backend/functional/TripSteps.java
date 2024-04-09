6fd8f0ee416e21e164eb34b24d6e775d205ec389
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-04-09 19:24:25 +0100

    Oops, I commented headless

--- HW1/backend/src/test/java/pt/ua/deti/tqs/backend/functional/TripSteps.java
+++ HW1/backend/src/test/java/pt/ua/deti/tqs/backend/functional/TripSteps.java
@@ -22,7 +22,7 @@ public class TripSteps {
     @Given("I navigate to {string}")
     public void iNavigateTo(String url) {
         FirefoxOptions options = new FirefoxOptions();
-        // options.addArguments("-headless");
+        options.addArguments("-headless");
 
         driver = new FirefoxDriver(options);
         driver.get(url);
